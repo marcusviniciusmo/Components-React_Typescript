de948c3d95925351b390fcef4720fd32fb6cdf29
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-08-11 20:29:49 +0100

    Component - Page Not Found - 001

---
+++ frontend/src/components/PageNotFound/Example001/index.tsx
@@ -0,0 +1,32 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles.css';
+
+function Example001() {
+  return (
+    <section className="page404-example001">
+      <div className='container'>
+        <div className='row'>
+          <div className='col-sm-12'>
+            <div className='col-sm-10 col-sm-offset-1 text-center'>
+              <div className='pageNotFoundBackground-example001'>
+                <h1 className='textCenter'>404</h1>
+              </div>
+
+              <div className='contantBox404-example001'>
+                <h3 className='h2'>Look like you're lost</h3>
+
+                <p>the page you are looking for not available!</p>
+                <a href="" className='link404-example001'>Go to Home</a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <span>Credits: codewithrandom</span>
+    </section>
+  );
+};
+
+export default Example001;
--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,11 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Example001  from 'components/PageNotFound/Example001';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route />
+        <Route path='/components/pageNotFound/001' element={<Example001 />} />
       </Routes>
     </BrowserRouter>
   );
