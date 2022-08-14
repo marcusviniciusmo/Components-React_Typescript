688e65880b8913d4a03f334fd0dfe985b8d89900
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-08-14 15:58:21 +0100

    Home view creation

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from 'views/Home';
 import Example001  from 'components/PageNotFound/Example001';
 
 function Router() {
   return (
     <BrowserRouter>
       <Routes>
+        <Route path='/' element={<Home />} />
         <Route path='/components/pageNotFound/001' element={<Example001 />} />
       </Routes>
     </BrowserRouter>
